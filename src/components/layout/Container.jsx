17256fb5356138cf104e2987fdb797354c4f6331
dragomir.p <dragomir.p@first.dev>
2025-12-06 22:35:08 +0200

    Ui fixes

--- src/components/layout/Container.jsx
+++ src/components/layout/Container.jsx
@@ -15,7 +15,7 @@ export default function Container({ children, className = '', onSwipeLeft, onSwi
 
   return (
     <main className="flex-1 overflow-y-auto">
-      <div className={`max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 ${className}`}>
+      <div className={`max-w-7xl mx-auto px-3 sm:px-6 lg:px-8 py-8 ${className}`}>
         {children}
       </div>
     </main>
