def43e559bbe59a0ce484f3263c0fa2982dfbeeb
dragomir.p <dragomir.p@first.dev>
2025-12-06 23:02:26 +0200

    Additional fixes

--- src/components/layout/Container.jsx
+++ src/components/layout/Container.jsx
@@ -6,7 +6,7 @@
 export default function Container({ children, className = '' }) {
   return (
     <main className="flex-1 overflow-y-auto">
-      <div className={`max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 py-8 ${className}`}>
+      <div className={`max-w-7xl mx-auto px-1 sm:px-6 lg:px-8 py-8 ${className}`}>
         {children}
       </div>
     </main>
